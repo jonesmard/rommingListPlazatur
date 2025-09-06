ffe77a3f7bd59d75270dcd29697f4cde52a92d85
Jones mardoqueu <65608034+jonesmard@users.noreply.github.com>
2025-09-06 18:56:33 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'dashboard-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
+    'https://riodorastro.com.br/wp-content/uploads/2021/08/logo-rio-do-rastro.png',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aberto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
